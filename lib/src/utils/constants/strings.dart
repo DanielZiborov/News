ed296141b9280caa7a37bd892686cedbd8fea001
DanielZiborov <ziborovdanila5@gmail.com>
2024-11-08 16:59:05 +0300

    определяем некоторые константы

---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,7 @@
+const String appTitle = "News";
+
+const String baseUrl = "https://newsapi.org/v2";
+const String defaultApiKey="YOUR_API_KEY";
+
+const String articlesTableName = "articles_table";
+const String databaseName = "app_database.db";
