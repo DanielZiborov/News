d694b59ed7ab728f6ad6e3add43c18104dd88157
DanielZiborov <ziborovdanila5@gmail.com>
2024-11-09 19:18:12 +0300

    данные новостной api сервис

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -1,7 +1,8 @@
 const String appTitle = "News";
 
-const String baseUrl = "https://newsapi.org/v2";
-const String defaultApiKey="YOUR_API_KEY";
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = '';
+const String defaultSources = 'bbc-news, abc-news, al-jazeera-english';
 
 const String articlesTableName = "articles_table";
 const String databaseName = "app_database.db";
