46017ea13f0a273fcb0fdc19d4f8828f361e7ec0
DanielZiborov <ziborovdanila5@gmail.com>
2024-11-11 18:11:39 +0300

    частично главный экран

--- lib/src/presentation/views/breaking_news_screen.dart
+++ lib/src/presentation/views/breaking_news_screen.dart
@@ -1,23 +1,79 @@
-import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:news/src/domain/models/article.dart';
+import 'package:news/src/presentation/cubits/remote_articles/remote_articles_cubit.dart';
 
-@RoutePage()
-class BreakingNewsScreen extends StatelessWidget {
+class BreakingNewsScreen extends HookWidget {
   const BreakingNewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final remoteArticlesCubit = BlocProvider.of<RemoteArticlesCubit>(context);
+    final scrollController = useScrollController();
+
+    useEffect(() {
+      scrollController.addListener(() {
+        remoteArticlesCubit.getBreakingNewsArticles();
+      });
+
+      return scrollController.dispose;
+    }, const []);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Example"),
-      ),
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          color: Colors.red,
+        title: const Text(
+          'Daily News',
+          style: TextStyle(color: Colors.black),
         ),
       ),
+      body: BlocBuilder<RemoteArticlesCubit, RemoteArticlesState>(
+        builder: (_, state) {
+          switch (state.runtimeType) {
+            case const (RemoteArticlesLoading):
+              return const Center(child: CupertinoActivityIndicator());
+            case const (RemoteArticlesFailed):
+              return const Center(child: Icon(Ionicons.refresh));
+            case const (RemoteArticlesSuccess):
+              return _buildArticles(
+                scrollController,
+                state.articles,
+                state.noMoreData,
+              );
+            default:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildArticles(
+    ScrollController scrollController,
+    List<Article> articles,
+    bool noMoreData,
+  ) {
+    return CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => ArticleWidget(
+              article: articles[index],
+            ),
+            childCount: articles.length,
+          ),
+        ),
+        if (!noMoreData)
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(top: 14, bottom: 32),
+              child: CupertinoActivityIndicator(),
+            ),
+          )
+      ],
     );
   }
 }
