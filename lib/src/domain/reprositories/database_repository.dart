9dca653baad9a759e9857e0db35b9fe0c909d3c0
DanielZiborov <ziborovdanila5@gmail.com>
2024-11-12 22:01:12 +0300

    репозитории бд

---
+++ lib/src/domain/reprositories/database_repository.dart
@@ -0,0 +1,7 @@
+import 'package:news/src/domain/models/article.dart';
+
+abstract class DatabaseRepository {
+  Future<List<Article>> getSavedArticles();
+  Future<void> saveArticle(Article article);
+  Future<void> removeArticle(Article article);
+}
